2c5a7c9b2526b63e99d16f7f7ae61e161eec95b5
jonleesy <65177718+jonleesy@users.noreply.github.com>
2020-11-20 14:50:42 +0000

    Update leaderboard.js

--- Database/connection/leaderboard.js
+++ Database/connection/leaderboard.js
@@ -1,6 +1,31 @@
-var firstName ="SELECT username FROM players JOIN (SELECT distinct score score3 FROM players ORDER BY score DESC LIMIT 2, 1) x ON highscore >= score3 ORDER by highscore DESC ;";
-var firstScore ="";
-var secondName ="SELECT score FROM players order by score desc limit 1,1";
-var secondScore ="SELECT name FROM players order by score desc limit 1,1";
-var thirdName ="";
-var thirdScore ="";
+var firstName ="SELECT score FROM players order by score desc limit 1,1;";
+var firstScore ="SELECT username FROM players order by score desc limit 0,1;";
+var secondName ="SELECT score FROM players order by score desc limit 1,1;";
+var secondScore ="SELECT username FROM players order by score desc limit 1,1;";
+var thirdName ="SELECT score FROM players order by score desc limit 2,1;";
+var thirdScore ="SELECT username FROM players order by score desc limit 2,1;";
+
+connection.query(firstName, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+connection.query(firstScore, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+connection.query(secondName, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+connection.query(secondScore, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+connection.query(thirdName, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+connection.query(thirdScore, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
