2078b3d0bff7a867f1ac826307216c16e11e222d
im91-ai <71633239+im91-ai@users.noreply.github.com>
2020-11-26 14:03:02 +0000

    Update dog.js

--- Database/dog.js
+++ Database/dog.js
@@ -1,3 +1,4 @@
+<script src="/socket.io/socket.io.js"></script
 const cvs = document.getElementById("dog");
 cvs.height = 640;
 cvs.width = 640;
@@ -33,6 +34,103 @@ right.src = "audio/right.mp3";
 left.src = "audio/left.mp3";
 down.src = "audio/down.mp3";
 
+      let socket = 0;
+      try {
+        //const url = window.location.origin || "http://localhost:8080";
+
+        const url =
+        // look at project video to do below:
+          window.location.origin || "https://zltog.sse.codesandbox.io/";
+        socket = io.connect(url);
+      } catch (err) {}
+
+      // random number between 0 to 1000
+      var uniqueid = Math.round(Math.random() * 1000);
+      // timestamp from server
+      var servertimestamp = 0;
+
+      function setContent() {
+        var xx = 100;
+        var yy = 100;
+        var found = -1;
+        for (let n = 0; n < 5; ++n) {
+          let idd = parseInt(document.getElementById("t" + n).innerHTML);
+          if (uniqueid == idd) {
+            found = n;
+          }
+        }
+        if (found != -1) {
+          xx = parseFloat(document.getElementById("i" + found).style.left);
+          yy = parseFloat(document.getElementById("i" + found).style.top);
+        }
+
+        /*
+	sending data to the server
+	*/
+        socket.emit("update", {
+          cmd: "update",
+          timestamp: servertimestamp,
+          uniqueid: uniqueid,
+          x: xx,
+          y: yy,
+          id: socket.id
+        });
+      }
+
+      /*
+receiving data from the server
+*/
+      socket.on("sync", function (data) {
+        let id = data.id;
+        let cmd = data.cmd;
+
+        if (cmd != "sync") return;
+
+        // put the data_from_file into #timestamp
+        $("#timestamp").html(data.timestamp);
+        // how many users do we have
+        $("#users").html("number users: " + data.users.length);
+
+        // This just displays the list of active users the server sent back to us
+        var userdata = "";
+        for (let n = 0; n < data.users.length; ++n) {
+          userdata += "ip: " + data.users[n].ip + "<br>";
+          userdata += "uniqueid: " + data.users[n].uniqueid + "<br>";
+          userdata += "x: " + data.users[n].x + ", ";
+          userdata += "y: " + data.users[n].y + "<br>";
+          userdata += "timestamp: " + data.users[n].timestamp + "<br>";
+        }
+        $("#userdata").html(userdata);
+
+        // This updates the other users positions on the screen
+        var cc = 1; // image 0 is reserved for player
+        for (
+          let n = 0;
+          n < 5;
+          ++n // 5 images
+        ) {
+          if (n < data.users.length) {
+            // only got 5 shapes to show
+            if (data.users[n].uniqueid == uniqueid) {
+              servertimestamp = data.timestamp;
+              continue;
+            }
+
+            // only 1 image for now, always update this image
+            document.getElementById("i" + cc).style.left = data.users[n].x;
+            document.getElementById("i" + cc).style.top = data.users[n].y;
+            document.getElementById("t" + cc).innerHTML =
+              data.users[n].uniqueid;
+          } else {
+            document.getElementById("i" + cc).style.left = -200;
+            document.getElementById("t" + cc).innerHTML = 99;
+          }
+          cc++;
+        }
+      });
+
+
+
 // create the Dog
 
 let dog = [];
@@ -48,6 +146,34 @@ let bone = {
     x : Math.floor(Math.random()*17+1) * box,
     y : Math.floor(Math.random()*15+3) * box
 }
+
+// create the grape/s 
+
+let grape1 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+let grape2 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+let grape3 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+let grape4 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+let grape5 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+let grape6 = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
 
 // create the scores var
 
@@ -93,6 +219,13 @@ function draw(){
     }
 
     ctx.drawImage(boneImg, bone.x, bone.y, 20, 20);
+
+    ctx.drawImage(grapeImage, grape1.x, grape1.y, 25, 25);
+    ctx.drawImage(grapeImage, grape2.x, grape2.y, 25, 25);
+    ctx.drawImage(grapeImage, grape3.x, grape3.y, 25, 25);
+    ctx.drawImage(grapeImage, grape4.x, grape4.y, 25, 25);
+    ctx.drawImage(grapeImage, grape5.x, grape5.y, 25, 25);
+    ctx.drawImage(grapeImage, grape6.x, grape6.y, 25, 25);
     
     // old head position
     let dogX = dog[0].x;
@@ -125,6 +258,57 @@ function draw(){
         x : dogX,
         y : dogY
     }
+
+    // if the dog eats the grape/s 
+
+    if(dogX == grape1.x && dogY == grape1.y || dogX == grape1.x-0.5*box && dogY == grape1.y){
+        score1--;
+        eat.play();
+        grape1 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
+    if(dogX == grape2.x && dogY == grape2.y || dogX == grape2.x-0.5*box && dogY == grape2.y){
+        score1--;
+        eat.play();
+        grape2 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
+    if(dogX == grape3.x && dogY == grape3.y || dogX == grape3.x-0.5*box && dogY == grape3.y){
+        score1--;
+        eat.play();
+        grape3 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
+    if(dogX == grape4.x && dogY == grape4.y || dogX == grape4.x-0.5*box && dogY == grape4.y){
+        score1--;
+        eat.play();
+        grape4 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
+    if(dogX == grape5.x && dogY == grape5.y || dogX == grape5.x-0.5*box && dogY == grape5.y){
+        score1--;
+        eat.play();
+        grape5 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
+    if(dogX == grape6.x && dogY == grape6.y || dogX == grape6.x-0.5*box && dogY == grape6.y){
+        score1--;
+        eat.play();
+        grape6 = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }
     
     
     // game over
@@ -147,3 +331,4 @@ function draw(){
 // call draw function every 100 ms
 
 let game = setInterval(draw,100);
+
